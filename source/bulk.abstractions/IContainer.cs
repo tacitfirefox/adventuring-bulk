da10a90744f95768b1696e8695e0e39f3434d5c1
Seth Jacobson-Pohutski <tacitfirefox@outlook.com>
2021-03-21 11:48:03 -0400

    Create IContainer.cs
    Adding blank interface code file to see if this helps a GitHub action error on dotnet restore.

---
+++ source/bulk.abstractions/IContainer.cs
@@ -0,0 +1,17 @@
+﻿namespace bulk.abstractions
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Defines a packable container of objects that can be stowed or carried.
+    /// </summary>
+    public interface IContainer : IEnumerable
+    {
+
+    }
+}
