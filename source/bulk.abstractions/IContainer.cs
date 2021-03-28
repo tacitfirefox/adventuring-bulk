4b76ce00798176e197f3f84464ae723b7370ad36
tacitfirefox <80607742+tacitfirefox@users.noreply.github.com>
2021-03-28 16:16:00 -0400

    Adds container interface and supporting enums. (#9)
    Also updates the class diagram, but not with any meaningful information yet.
    
    Co-authored-by: tacitfirefox <tacitfirefox@protonmail.com>

--- source/bulk.abstractions/IContainer.cs
+++ source/bulk.abstractions/IContainer.cs
@@ -1,17 +1,41 @@
-﻿namespace bulk.abstractions
+﻿//==============================================
+// IContainer.cs
+// tacitfirefox
+//==============================================
+
+namespace bulk.abstractions
 {
-    using System;
     using System.Collections;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    using UnitsNet;
 
     /// <summary>
     /// Defines a packable container of objects that can be stowed or carried.
     /// </summary>
     public interface IContainer : IEnumerable
     {
+        /// <summary>
+        /// Gets the mass of the container.
+        /// </summary>
+        public Mass Mass { get; }
 
+        /// <summary>
+        /// Gets the volume of the container.
+        /// </summary>
+        public Volume Volume { get; }
+
+        /// <summary>
+        /// Gets the slot(s) this container can be equipped to on a person.
+        /// </summary>
+        public PersonalEquipmentSlots EquipmentSlot { get; }
+
+        /// <summary>
+        /// Gets the storage option(s) this container can be placed.
+        /// </summary>
+        public StorageOptions StorageOptions { get; }
+
+        /// <summary>
+        /// Gets the number of inventory slots this container holds.
+        /// </summary>
+        public uint Slots { get; }
     }
 }
